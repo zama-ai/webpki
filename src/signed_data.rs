9f0ad370f2c3f16ee25cc85b5a9f77804bd673dc
Nikita Frolov <nf@mkmks.org>
2026-03-20 10:17:36 +0100

    Revert "display certificate signature alg id correctly"
    This reverts commit 0b5ee7b1de0b2d87457c0bf1cc93f8e442f3a3c1.

--- src/signed_data.rs
+++ src/signed_data.rs
@@ -230,7 +230,7 @@ pub(crate) fn verify_signature(
         return Err(Error::UnsupportedSignatureAlgorithmForPublicKeyContext(
             UnsupportedSignatureAlgorithmForPublicKeyContext {
                 #[cfg(feature = "alloc")]
-                signature_algorithm_id: signature_alg.public_key_alg_id().as_ref().to_vec(),
+                signature_algorithm_id: signature_alg.signature_alg_id().as_ref().to_vec(),
                 #[cfg(feature = "alloc")]
                 public_key_algorithm_id: spki.algorithm_id_value.as_slice_less_safe().to_vec(),
             },
